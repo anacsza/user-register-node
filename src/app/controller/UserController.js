44cd65fed67853ea4ba3dd2c2dde336723852b0a
anacsza <ana.caroline.souza09@gmail.com>
2020-07-30 22:09:59 -0300

    queue configured

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,5 +1,8 @@
 import passwordGenerator from 'password-generator';
 import Mail from '../lib/Mail';
+
+import Queue from '../lib/Queue';
+
 export default {
     async store(req, res) {
         const { name, email } = req.body;
@@ -8,6 +11,7 @@ export default {
             email,
             password: passwordGenerator(15, false)
         };
+        await Queue.add('RegistrationMail', { user });
         return res.json(user);
     }
 }
--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import 'dotenv/config';
 import express from 'express';
 
 import UserController from './app/controller/UserController';
+import Queue from './app/lib/Queue';
 
 const app = express();
 
+console.log(Queue);
+
 app.use(express.json());
+
+app.post('/users', UserController.store);
 
 app.listen(process.env.PORT, () => {
     console.log('Server running on ' + process.env.PORT)
 });
-
-app.post('/users', UserController.store);
